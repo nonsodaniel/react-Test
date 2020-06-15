8c88441923e2859f590cf14864d683ade22462c4
nonsodaniel <nonsodaniel07@gmail.com>
2020-06-15 02:51:06 +0200

    Application reset

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import Navbar from "./components/layouts/Navbar";
-// import Home from "./components/home/Home";
-// import Footer from "./components/layouts/Footer";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import TourGuideRegister from "./components/auth/TourGuideRegister";
-// import Header from "./components/layouts/Header";
-import Cart from "./components/carts/Cart";
-import { Home } from "./components/home/Home";
-import Checkout from "./components/checkout/Checkout";
-import LoadingOverlay from 'react-loading-overlay';
 
 class App extends Component {
 
@@ -25,27 +14,9 @@ class App extends Component {
   }
   render() {
     return (
-      <LoadingOverlay
-        active={false}
-        spinner
-        text='Loading your content...'
-      >
-        <BrowserRouter>
-          <div className="app">
-            {/* <Navbar /> */}
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/register/tour-guide" component={TourGuideRegister} />
-              <Route exact path="/cart" component={Cart} />
-              <Route exact path="/checkout" component={Checkout} />
-
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </LoadingOverlay>
-
+      <div>
+        <button className="btn btn-danger">hell</button>
+      </div>
     );
   }
 }
